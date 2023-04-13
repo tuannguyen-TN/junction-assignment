672f591823a72c98eccc35a9658d99dede4de563
Tuan Nguyen <tuan.nguyen@tuni.fi>
2023-04-13 21:49:55 +0300

    complete teams info display

--- join-a-team-site/src/App.jsx
+++ join-a-team-site/src/App.jsx
@@ -4,14 +4,14 @@ import Teams from './Teams'
 import axios from 'axios'
 
 function App() {
-	// const [teams, setTeams] = useState([])
+	const [teams, setTeams] = useState([])
 
-	// useEffect(() => {
-	// 	axios
-	// 		.get('http://localhost:5000/teams')
-	// 		.then((res) => setTeams(res.data))
-	// 		.catch((err) => console.log(err))
-	// }, [])
+	useEffect(() => {
+		axios
+			.get('http://localhost:5000/teams')
+			.then((res) => setTeams(res.data))
+			.catch((err) => console.log(err))
+	}, [])
 
 	return (
 		<div className="app">
@@ -19,9 +19,11 @@ function App() {
 				<h1>JOIN A TEAM</h1>
 				<p>Apply for a role that suits you the most and join a team.</p>
 			</div>
-			{/* <Teams team={teams[0]} />
-			<Teams team={teams[1]} /> */}
-			<Teams />
+			<div className="team-container">
+				{teams.map((item, index) => (
+					<Teams key={index} team={item} />
+				))}
+			</div>
 		</div>
 	)
 }
--- join-a-team-site/src/Teams.jsx
+++ join-a-team-site/src/Teams.jsx
@@ -4,68 +4,57 @@ import showMoreLogo from './assets/down-arrow.svg'
 import showLessLogo from './assets/up-arrow.svg'
 import axios from 'axios'
 
-function Teams() {
+function Teams(props) {
 	const [showMore, setShowMore] = useState(false)
-	const [teams, setTeams] = useState([])
-
-	useEffect(() => {
-		axios
-			.get('http://localhost:5000/teams')
-			.then((res) => setTeams(res.data))
-			.catch((err) => console.log(err))
-	}, [])
+	const team = props.team
 
 	const toggleShowMore = () => {
 		setShowMore(!showMore)
 	}
 
 	return (
-		<div className="team-container">
-			{teams.map((item, index) => (
-				<div key={index} className={showMore ? 'team-item-extended' : 'team-item'}>
-					<div className="team-title">
-						<h1>{item.title.toUpperCase()}</h1>
-						<h2 className="challenge">#{item.challenge}</h2>
-					</div>
-					<p>{item.idea}</p>
-					{showMore ? <p>{item.description}</p> : null}
-					<h2 className="section-title">WE ARE LOOKING FOR</h2>
-					<div className="vacancy-container">
-						{item.vacancies.map((vacancy, index) => (
-							<a href="/" key={index} className="vacancy-item">
-								<h2 className="vacancy">{vacancy}</h2>
-							</a>
+		<div className={showMore ? 'team-item-extended' : 'team-item'}>
+			<div className="team-title">
+				<h1>{team.title.toUpperCase()}</h1>
+				<h2 className="challenge">#{team.challenge}</h2>
+			</div>
+			<p>{team.idea}</p>
+			{showMore ? <p>{team.description}</p> : null}
+			<h2 className="section-title">WE ARE LOOKING FOR</h2>
+			<div className="vacancy-container">
+				{team.vacancies.map((vacancy, index) => (
+					<a href="/" key={index} className="vacancy-item">
+						<h2 className="vacancy">{vacancy}</h2>
+					</a>
+				))}
+			</div>
+			{showMore ? (
+				<div>
+					<h2 className="section-title">WHO WE ARE</h2>
+					<div className="member-container">
+						{team.members.map((member, index) => (
+							<a href="/" key={index} className="member-item"></a>
 						))}
 					</div>
-					{showMore ? (
-						<div>
-							<h2 className="section-title">WHO WE ARE</h2>
-							<div className="member-container">
-								{item.members.map((member, index) => (
-									<a href="/" key={index} className="member-item"></a>
-								))}
+					<div className="member-info-container">
+						{team.members.map((member, index) => (
+							<div key={index} className="member-info-item">
+								<p>{member.name}</p>
+								<p>{member.role}</p>
 							</div>
-							<div className="member-info-container">
-								{item.members.map((member, index) => (
-									<div key={index} className="member-info-item">
-										<p>{member.name}</p>
-										<p>{member.role}</p>
-									</div>
-								))}
-							</div>
-						</div>
-					) : null}
-					{showMore ? (
-						<button className="show-less-logos" onClick={toggleShowMore}>
-							<img src={showLessLogo} className="logo" alt="Up arrow" />
-						</button>
-					) : (
-						<button className="show-more-logos" onClick={toggleShowMore}>
-							<img src={showMoreLogo} className="logo" alt="Down arrow" />
-						</button>
-					)}
+						))}
+					</div>
 				</div>
-			))}
+			) : null}
+			{showMore ? (
+				<button className="show-less-logos" onClick={toggleShowMore}>
+					<img src={showLessLogo} className="logo" alt="Up arrow" />
+				</button>
+			) : (
+				<button className="show-more-logos" onClick={toggleShowMore}>
+					<img src={showMoreLogo} className="logo" alt="Down arrow" />
+				</button>
+			)}
 		</div>
 	)
 }
