ec5ee75a50235d2cbfd3ddd184e2971f6803b80b
Tuan Nguyen <tuan.nguyen@tuni.fi>
2023-04-14 12:02:20 +0300

    add info page for members

--- join-a-team-site/src/routes/Member.jsx
+++ join-a-team-site/src/routes/Member.jsx
@@ -1,10 +1,39 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import './Member.css'
 
 function Member() {
-	const params = useParams()
+	const { memberName } = useParams()
+	const [memberInfo, setMemberInfo] = useState({})
 
-	return <div>{Object.entries(params)}</div>
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/members/${memberName}`)
+			.then((res) => setMemberInfo(res.data))
+			.catch((err) => console.log(err))
+	}, [])
+
+	return (
+		<div className="info-container">
+			<div className="member-title">
+				<p className="member-name">{memberInfo.name}</p>
+				<p className="member-role">/ {memberInfo.role}</p>
+			</div>
+			<p className="member-email">{memberInfo.email}</p>
+			<div className="specialized-info">
+				<div className="member-img"></div>
+				<div className="member-skills">
+					<h2>Skills</h2>
+					<p>{memberInfo.about}</p>
+				</div>
+				<div className="member-about">
+					<h2>About Me</h2>
+					<p>{memberInfo.about}</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Member
